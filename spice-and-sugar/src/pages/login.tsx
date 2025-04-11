7e8c80d6258482739f35141a6258cc263c6aabd5
Sahar Abdollahi <sahar.abdollahi@torontomu.ca>
2025-04-11 15:48:02 -0400

    refactor: increase top margin in LoginPage component

--- spice-and-sugar/src/pages/login.tsx
+++ spice-and-sugar/src/pages/login.tsx
@@ -77,7 +77,7 @@ const LoginPage = () => {
           justifyContent: "center",
           gap: 1,
           p: 2,
-          mt: "6rem",
+          mt: "8rem",
           borderRadius: "20px",
           backgroundColor: "rgba(255, 255, 255, 0.25)",
           backdropFilter: "blur(10px)",
