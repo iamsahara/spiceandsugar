02c9236ffaa45c728cff412dcf8b989690bd913d
Sahar Abdollahi <sahar.abdollahi@torontomu.ca>
2025-04-11 14:35:03 -0400

    refactor: remove unused imports and photosList array in Hero component

--- spice-and-sugar/src/components/Hero.tsx
+++ spice-and-sugar/src/components/Hero.tsx
@@ -1,18 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
 import { Box, Button, Stack } from "@mui/material";
-
-const photosList = [
-  "/IMG_6080.jpg",
-  "/IMG_6091.jpg",
-  "/IMG_6092.jpg",
-  "/IMG_6082.jpg",
-  "/IMG_6086.jpg",
-  "/IMG_6087.jpg",
-  "/IMG_6084.jpg",
-  "/IMG_6079.jpg",
-  "/IMG_6091.jpg"
-];
 
 export default function Hero() {
   return (
