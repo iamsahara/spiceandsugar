0b8003931bf6748c000aeb0d2365c532666f4831
Sahar Abdollahi <sahar.abdollahi@torontomu.ca>
2025-04-10 23:28:18 -0400

    console log

--- spice-and-sugar/src/pages/api/users.ts
+++ spice-and-sugar/src/pages/api/users.ts
@@ -3,8 +3,9 @@ import supabase from "../../lib/supabase";
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log(req)
   if (req.method !== "POST") {
-    console.log(req)
+   
     return res.status(405).json({ message: "Method Not Allowed" });
   }
 
